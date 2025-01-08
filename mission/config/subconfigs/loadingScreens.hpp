b22017a9009bb0780e003b9a9ff4d5db9730236e
Hamlock-maneuverr <cerebrallolzy1@gmail.com>
2025-01-08 00:19:26 -0600

    Signs, loading screen.

--- mission/config/subconfigs/loadingScreens.hpp
+++ mission/config/subconfigs/loadingScreens.hpp
@@ -1,7 +1,7 @@
 
 mainWelcome = "custom\loadingscreens\BN_Welcome_202406.paa";
 patreonBasic = "custom\loadingscreens\Patreon_1_welcome.paa";
-patreonSupporters = "custom\loadingscreens\Patreon_2_supporters_2024_08_18.paa";
+patreonSupporters = "custom\loadingscreens\Patreon_2_supporters_2025_01_07.paa";
 
 images[] =
 {
@@ -10,5 +10,5 @@ images[] =
 	"\vn\objects_f_vietnam\civ\signs\data\billboards\vn_ui_billboard_03_ca.paa",
 	"custom\loadingscreens\BN_Welcome_202406.paa",
 	"custom\loadingscreens\Patreon_1_welcome.paa",
-	"custom\loadingscreens\Patreon_2_supporters_2024_08_18.paa"
+	"custom\loadingscreens\Patreon_2_supporters_2025_01_07.paa"
 };
