c39ba45577deab647ea4e12c039958939606a9b9
Hamlock-maneuverr <cerebrallolzy1@gmail.com>
2025-01-19 11:09:15 -0600

    crates stuff

--- mission/config/logistics.hpp
+++ mission/config/logistics.hpp
@@ -369,6 +369,9 @@ class vn_logistics
             rotation_offset = 0;
         };
 		//US Ammo
+		class Box_NATO_AmmoOrd_F : Box_NATO_AmmoOrd_F{};
+		class Box_NATO_Support_F : Box_NATO_Support_F {};
+		class Box_NATO_Ammo_F : Box_NATO_Ammo_F {};
 		class vn_b_ammobox_supply_01 : vn_b_ammobox_sog {};
 		//Medical supplies
 		class vn_b_ammobox_supply_03 : vn_b_ammobox_sog {};
