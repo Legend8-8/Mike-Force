cef07f666129e2bef76dcb35f926bdc32b2c5aca
Legend8-8 <nickdevereux1988@gmail.com>
2025-02-04 21:22:21 +0000

    Default Traits - Muskets now engineer. (#416)
    * Modules: PhotoCamera: Add enable screenshots module.
    
    - Added `vn_module_photo_camera_enable_screenshots` logic module.
    - Incremented item count and IDs in `mission.sqm` accordingly.
    
    * Config: VehicleRespawn: Add TigerForce to limited armor lock.
    
    - Added "TigerForce" to the `LOCKED_LIMITED_ARMOR` group.
    - Allows TigerForce  access to limited armor such as statics etc.
    
    * Default Traits - Muskets now engineer.
    
    Added engineer to Muskets default traits as per request.
    
    ---------
    
    Co-authored-by: Legend8-8 <Legend88.dev@gmail.com>

--- mission/config/subconfigs/teams.hpp
+++ mission/config/subconfigs/teams.hpp
@@ -462,7 +462,7 @@ class Muskets
         camouflageCoef = 0.8;
         audibleCoef = 0.6;
         loadCoef = 1;
-        engineer = false;
+        engineer = true;
         explosiveSpecialist = false;
         medic = false;
         UAVHacker = false;
